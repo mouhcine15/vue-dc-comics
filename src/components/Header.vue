236a6b26b7ea559b43f606799eeafa501bfdfdb1
mouhcine <mouhcine15.00@gmail.com>
2022-06-08 16:02:54 +0200

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,87 @@
+<template>
+    <header>
+        <div>
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+        <div id="menu">
+            <ul>
+                <li v-for="(link, i) in links" :key="i">
+                    <a :href="link.url">{{link.text}}</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'myHeader',
+  data(){
+      return {
+          links: [
+             {
+                 text: "CHARACTERS",
+                 url: "#"
+             }, 
+             {
+                 text: "COMICS",
+                 url: "#"
+             }, 
+             {
+                 text: "MOVIES",
+                 url: "#"
+             },
+             {
+                 text: "TV",
+                 url: "#"
+             }, 
+             {
+                 text: "GAMES",
+                 url: "#"
+             },
+             {
+                 text: "COLLECTIBLES",
+                 url: "#"
+             }, 
+             {
+                 text: "VIDEO",
+                 url: "#"
+             },
+             {
+                 text: "FANS",
+                 url: "#"
+             }, 
+             {
+                 text: "NEWS",
+                 url: "#"
+             },
+             {
+                 text: "SHOP",
+                 url: "#"
+             }
+          ]
+      }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    li {
+        display: inline;
+        padding: 10px;
+    }
+
+    a {
+        color: black;
+        text-decoration: none;
+    }
+
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: white;
+    }
+</style>
